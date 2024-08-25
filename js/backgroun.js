f6ff1de6ffdb51a7b3429d3f1d01e8866e6c55ac
hizoo66 <162318690+hizoo66@users.noreply.github.com>
2024-08-25 19:43:19 +0900

    final

---
+++ js/backgroun.js
@@ -0,0 +1,11 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundSize = 'cover'; 
+document.body.style.backgroundPosition = 'center'; 
+document.body.style.backgroundRepeat = 'no-repeat';
